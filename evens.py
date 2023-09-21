35537705d464400878ecdbdd1bec953437effdd1
Godinhoweverson <>
2023-09-21 21:19:05 +0000

    Create the first python test

--- evens.py
+++ evens.py
@@ -1,5 +1,10 @@
 def even_number_of_evens(numbers):
-    return True
+    if isinstance(numbers, list):
+        if numbers == []:
+            return False
+    else:
+        raise  TypeError("A list was not passed into the function")
+
 
 if __name__ == '__main__':
     print(even_number_of_evens(5))
--- test_evens.py
+++ test_evens.py
@@ -2,7 +2,12 @@ import unittest
 from evens import  even_number_of_evens
 
 class testEvens(unittest.TestCase):
-    pass
+
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(typeError, even_number_of_evens, 4)
+    
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
 
 if __name__ == '__main__':
     unittest.main()
